fc605bc98a67dde24976b42887ef319e1f44c03b
garamonfok <serra.francois@gmail.com>
2010-05-13 19:29:13 +0200

    include translate

--- 2_align_proteins/aligner.py
+++ 2_align_proteins/aligner.py
@@ -4,7 +4,6 @@
 
 from optparse import OptionParser
 from re import match, split
-from translate import translate
 from sys import stderr
 from subprocess import Popen, PIPE
 
@@ -52,7 +51,7 @@ def main():
     if proc.communicate()[1] is not None:
         print >> stderr, proc.communicate()[0]
         exit('\nERROR: runninge muscle')
-    
+
     ###########
     # TRIM SEQS
     if opts.trimseq:
@@ -100,7 +99,8 @@ def main():
                       ], stdout=PIPE)
 
         (keeplist, err) = proc.communicate()
-        if err is not None: exit('ERROR: trimming columns.')
+        if err is not None:
+            exit('ERROR: trimming columns.')
 
         keeplist = str (keeplist).strip().split(', ')
 
@@ -214,6 +214,72 @@ def divide(seq, size=3, rm_cod = True):
             codons.append(seq[i:i+size])
     return codons
 
+def translate(sequence, stop=False):
+    '''
+    little function to translate DNA to protein...
+    from: http://python.genedrift.org/
+    '''
+    #dictionary with the genetic code
+    gencode = {
+    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 
+    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+    'TAC':'Y', 'TAT':'Y', 'TAA':'0', 'TAG':'0',
+    'TGC':'C', 'TGT':'C', 'TGA':'0', 'TGG':'W',
+    '---':'-'
+    }
+    ambig = {'Y':['A', 'G'], 'R':['C', 'T'], 'M':['G', 'T'], 'K':['A', 'C'], \
+             'S':['G', 'C'],'W':['A', 'T'], 'V':['C', 'G', 'T'], \
+             'H':['A', 'G', 'T'], 'D':['A', 'C', 'T'], 'B':['A', 'C', 'G'], \
+             'N':['A', 'C', 'G', 'T']}
+    proteinseq = ''
+    #loop to read DNA sequence in codons, 3 nucleotides at a time
+    for n in range(0, len(sequence), 3):
+        #checking to see if the dictionary has the key
+        try:
+            proteinseq += gencode[sequence[n:n+3]]
+        except KeyError:
+            if len (sequence[n:n+3])< 3 :
+                break
+            newcod = []
+            for nt in sequence[n:n+3]:
+                if ambig.has_key(nt):
+                    newcod.append(ambig[nt])
+                else :
+                    newcod.append(list (nt))
+            aa = ''
+            for nt1 in newcod[0]:
+                for nt2 in newcod[1]:
+                    for nt3 in newcod[2]:
+                        try:
+                            if aa == '':
+                                aa  = gencode[nt1+nt2+nt3]
+                            elif gencode[nt1+nt2+nt3] != aa:
+                                aa = 'X'
+                                break
+                        except KeyError:
+                            aa = 'X'
+                            break
+            proteinseq += aa
+    #return protein sequence
+    if stop:
+        if proteinseq.endswith('0'):
+            return proteinseq[:-1]
+        else:
+            return proteinseq
+    else:
+        return proteinseq
 
 def get_options():
     '''
