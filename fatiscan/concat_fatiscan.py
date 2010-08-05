722e676bba2baff4ce03d882ffe11caa88158911
francisco <francisco@mem13.(none)>
2010-08-05 16:51:15 +0200

    start with fatiscan man!!

---
+++ fatiscan/concat_fatiscan.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#        Author: Francois-Jose Serra
+# Creation Date: 2010/08/05 14:46:46
+
+
+import sys,os,re
+
+
+
+def main():
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    exit(main())
